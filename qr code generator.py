7c38ec01582d096ce3851e24e53177f4dfd559ea
R.Kantharuban <101087213+Mr-R-Kantharuban@users.noreply.github.com>
2022-03-08 09:48:38 +0530

    Update qr code generator.py

--- qr code generator.py
+++ qr code generator.py
@@ -1,5 +1,5 @@
-import qrcode
+import qrcode #lib for qr generation
 
 
-image = qrcode.make("https://www.google.com/maps/@10.3775489,78.804495,43m/data=!3m1!1e3")
-image.save('location1.jpg')
+image = qrcode.make("link for generation of the qr")
+image.save('image name.jpg')
